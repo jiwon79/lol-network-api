47adb7cd807358e3f6c976f607d67875fbd541ce
jiwon79 <19-079@ksa.hs.kr>
2021-07-05 20:33:49 +0900

    test: Test heroku CD

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ class Item(BaseModel):
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"Hello": "Test"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
