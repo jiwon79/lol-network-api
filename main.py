870deaed642656d4a7fd74bc32e1cafdb638bc43
jiwon79 <19-079@ksa.hs.kr>
2021-07-05 20:36:21 +0900

    test: Test git branch

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ class Item(BaseModel):
 
 @app.get("/")
 def read_root():
-    return {"Hello": "Test"}
+    return {"Hello": "TestDev"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
