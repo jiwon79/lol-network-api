e68c5028d61eea05dc8a3b13c00b2621339b5b63
jiwon79 <19-079@ksa.hs.kr>
2021-07-08 21:45:21 +0900

    fix: Change return game data

--- app/modules/opgg.py
+++ app/modules/opgg.py
@@ -3,31 +3,29 @@ from bs4 import BeautifulSoup
 from pprint import pprint
 LIMIT = -1
 
-def getAGameData(log, user_name):
+def getAGameData(log, user_name, summonerId):
     game_data = {
         'id': 0,
         'time': 0,
         'player': user_name,
         'result': '',
+        'summonerId': summonerId,
         'team': []
     }
-    game_data['id'] = log.select_one('.GameItem')['data-game-id']
-    game_data['time'] = log.select_one('.GameItem')['data-game-time']
+
+    gameItem = log.select_one('.GameItem')
+    game_data['id'] = gameItem['data-game-id']
+    game_data['time'] = gameItem['data-game-time']
+    game_data['result'] = gameItem['data-game-result']
 
     for team in log.select('.Team'):
         if (team.select('.Requester') != []):
             for summoner in team.select('.Summoner'):
                 name = summoner.select_one('.SummonerName > a').get_text()
                 if (user_name.replace(" ","") != name.replace(" ","")):
-                    game_data['team'].append(name)
-    
-    result =  log.select_one('.GameResult').get_text()
-    if ('victory' in result):
-        game_data['result'] = 'Victory'
-    else:
-        game_data['result'] = 'Defeat'
-        
+                    game_data['team'].append(name)        
     return game_data
+
 
 def getUserAllGameData(user_name: str):
     count = 0
@@ -44,11 +42,11 @@ def getUserAllGameData(user_name: str):
         if soup.select_one('.SummonerNotFoundLayout') is not None:
             return {}
 
-        summonerId = soup.select_one('.GameListContainer')['data-summoner-id']
+        summonerId = int(soup.select_one('.GameListContainer')['data-summoner-id'])
         logs = soup.select("div.GameItemWrap")
 
         for log in logs:
-            game_data = getAGameData(log, user_name)
+            game_data = getAGameData(log, user_name, summonerId)
             game_data_list.append(game_data)
 
         # while no information, requests matches data
@@ -70,9 +68,9 @@ def getUserAllGameData(user_name: str):
             logs = more_soup.select("div.GameItemWrap")
                 
             for log in logs:
-                game_data = getAGameData(log, user_name)
+                game_data = getAGameData(log, user_name, summonerId)
                 game_data_list.append(game_data)
-        # pprint(game_data_list)
+        pprint(game_data_list)
         return game_data_list
             
     else:
