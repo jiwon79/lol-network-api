476fcd25243a6eb3b8c683ed5b507b110818f3d3
jiwon79 <19-079@ksa.hs.kr>
2021-07-16 00:41:15 +0900

    fix: Add CORS settings

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ origins = [
     'http://localhost:3000',
     'http://localhost:3001',
     'http://localhost:3002',
+    'http://localhost:5500',
+    'http://localhost:5501',
     'http://localhost:8000',
 ]
 
